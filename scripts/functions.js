2e315d9733e00a3899f6dc45d0c6f74ccb4222dc
Laura López <lauralopez166@gmail.com>
2023-11-08 10:48:19 +0100

    guardado mitad del proyecto

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,20 +1,39 @@
 export let nombre;
-export let apellidos;
-export let fecha;
+let apellidos;
+let fecha;
+
 export let nombreCompleto;
 export let nombreCompletoLonguitud;
+export let primero;
+export let ultimo;
+export let quitar;
 
-export const saludo = () =>{
-   nombre= prompt("Hola, dígame su nombre");
-   while(nombre === undefined || nombre === ""){
-    nombre=prompt("Introduzca un nombre válido");
-}
-    apellidos= prompt("Dígame sus apellidos");
-    while(apellidos === undefined ||apellidos === ""){
-    apellidos=prompt("Introduzca valores válidos");
-}
-}
-export const longitud = () =>{
-    nombreCompleto = nombre + " " + apellidos;
-    nombreCompletoLonguitud = nombreCompleto.length; 
-}
+export const saludo = () => {
+  nombre = prompt("Hola, dígame su nombre");
+  while (nombre === undefined || nombre === "") {
+    nombre = prompt("Introduzca un nombre válido");
+  }
+  apellidos = prompt("Dígame sus apellidos");
+  while (apellidos === undefined || apellidos === "") {
+    apellidos = prompt("Introduzca valores válidos");
+  }
+};
+export const longitud = () => {
+  nombreCompleto = nombre + " " + apellidos;
+  nombreCompletoLonguitud = nombreCompleto.length;
+};
+export const primeraLetra = () => {
+  primero = nombreCompleto.toUpperCase().indexOf("A", 0) + 1;
+  if (primero === 0) {
+    primero = "Ninguna, porque no contiene A";
+  }
+};
+export const ultimaLetra = () => {
+  ultimo = nombreCompleto.toUpperCase().lastIndexOf("A", nombreCompleto.length) + 1;
+  if (ultimo === 0) {
+    ultimo = "Ninguna, porque no contiene A";
+  }
+};
+export const quitarLetras = () => {
+    quitar = nombreCompleto.slice(3);
+};
--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,33 @@
-import { longitud, saludo } from "./functions.js";
+import { quitarLetras,ultimaLetra, primeraLetra, longitud, saludo } from "./functions.js";
 
-import {nombre } from "./functions.js";
+import { nombre } from "./functions.js";
+import { nombreCompleto } from "./functions.js";
 import { nombreCompletoLonguitud } from "./functions.js";
+import { primero } from "./functions.js";
+import { ultimo } from "./functions.js";
+import { quitar } from "./functions.js";
 
-document.addEventListener("DOMContentLoaded", function(){
-    saludo();
-    longitud();
-    write();
+document.addEventListener("DOMContentLoaded", function () {
+  saludo();
+  longitud();
+  primeraLetra();
+  ultimaLetra();
+quitarLetras();
+  write();
 });
-const write = () =>{
-        document.open();
-        document.write(`<p>Buenos días, <b>${nombre}</b></p>`)
-        document.write(`<p>Tu nombre tiene <b>${nombreCompletoLonguitud}</b> caracteres, incluidos espacios.</p>`)
-        document.close();
-}
+const write = () => {
+  document.open();
+  document.write(`<p>Buenos días, <b>${nombre}</b></p>`);
+  document.write(
+    `<p>Tu nombre tiene <b>${nombreCompletoLonguitud}</b> caracteres, incluidos espacios.</p>`
+  );
+  document.write(
+    `<p>La <b>primera letra 'A'</b> de tu nombre completo está en la posición <b>${primero}</b></p>`
+  );
+  document.write(
+    `<p>La <b>última letra 'A'</b> de tu nombre completo está en la posición <b>${ultimo}</b></p>`
+  );
+  document.write(`<p>Tu nombre <b>menos las tres primeras letras es ${quitar}</b></p>`);
+  document.write(`<p>Tu nombre todo en <b>MAYÚSCULAS es <b>${nombreCompleto.toUpperCase()}</b></p>`);
+  document.close();
+};
