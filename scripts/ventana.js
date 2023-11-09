53c9ff1ece32dad8a4ecc4143fe120429c3635b3
Laura López <lauralopez166@gmail.com>
2023-11-09 19:00:31 +0100

    subventana acabada

---
+++ scripts/ventana.js
@@ -0,0 +1,10 @@
+document.getElementById("urlActual").textContent = window.location.href;
+document.getElementById("protocolo").textContent = window.location.protocol;
+
+const cerrarVentana = document.getElementById("cerrarVentana");
+
+const close = () => {
+    window.close("http://127.0.0.1:5500/ventanaNueva.html");
+};
+
+cerrarVentana.addEventListener("click", close);
